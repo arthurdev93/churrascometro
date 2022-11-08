2050fa3978fc5ce35149e15e9e650608758d4004
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2022-11-07 21:35:41 -0300

    ++

---
+++ script.js
@@ -0,0 +1,5 @@
+//      REGRA DE NEGÓCIO
+// Carne - 400g por pessoa + de 6 horas = 650g
+// Cerveja - 1200ml por pessoa + de 6 horas = 2000 ml
+// Refrigerante/agua - 1000 ml por pessoa + 6 horas = 1500 ml
+// Crianças valem por 0.5 
