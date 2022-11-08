ed51e613faa6ecbebd8278ab3ba97ce69d7a1cac
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2022-11-08 09:55:00 -0300

    ++

--- script.js
+++ script.js
@@ -4,7 +4,51 @@
 // Refrigerante/agua - 1000 ml por pessoa + 6 horas = 1500 ml
 // Crianças valem por 0.5
 
-//abaixo, função zerar opacidade do card
-function card(container) {
-    container.style.opacity = "none";
-} 
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular() {
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value; 
+    
+    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao)/2 * criancas);
+    let qdtTotalCerveja = cervejaPP(duracao) * adultos;
+    let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao)/2 * criancas);
+
+    resultado.innerHTML = `<p>${qdtTotalCarne / 1000}kg de Carne</p>`
+    resultado.innerHTML += `<p>${qdtTotalCerveja / 1000}l de Cerveja</p>`
+    resultado.innerHTML += `<p>${qdtTotalBebidas / 1000}l de Refrigerante/Água</p>`
+
+}
+
+function carnePP(duracao) {
+    let carne = 400;
+    if(duracao >= 6) {
+        return 650;
+    } else {
+        return 400;
+    } 
+}
+
+function cervejaPP(duracao) {
+    let carne = 400;
+    if(duracao >= 6) {
+        return 2000;
+    } else {
+        return 1200;
+    }  
+}
+
+function bebidasPP(duracao) {
+    let carne = 400;
+    if(duracao >= 6) {
+        return 1500;
+    } else {
+        return 1000;
+    }  
+}
