eddb938d17d70b4f2c570fb8460a3be51a42760c
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2022-11-07 22:09:14 -0300

    ++

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 // Carne - 400g por pessoa + de 6 horas = 650g
 // Cerveja - 1200ml por pessoa + de 6 horas = 2000 ml
 // Refrigerante/agua - 1000 ml por pessoa + 6 horas = 1500 ml
-// Crianças valem por 0.5 
+// Crianças valem por 0.5
+function card(this) {
+    let c = document.getElementsByClassName("container");
+    c.style.opacity = "none";
+} 
